398d01bce18c021f9ee55fac73c116aef81bbc53
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-05-12 07:43:09 +0000

    GET / redirects to /specs
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/js_test_core/trunk@1572 af276e61-6b34-4dac-905b-574b5f35ef33

--- spec/unit/js_spec/resources/web_root_spec.rb
+++ spec/unit/js_spec/resources/web_root_spec.rb
@@ -9,50 +9,89 @@ module JsTestCore
       end
 
       describe "#locate" do
-        it "when passed 'core', returns a Dir representing the JsTestCore core directory" do
-          runner = web_root.locate('core')
-          runner.should == Resources::Dir.new(JsTestCore::Server.core_path, '/core')
+        describe "when passed ''" do
+          it "returns self" do
+            web_root.locate('').should == web_root
+          end
         end
 
-        it "when passed 'implementations', returns a Dir representing the javascript implementations directory" do
-          runner = web_root.locate('implementations')
-          runner.should == Resources::Dir.new(JsTestCore::Server.implementation_root_path, '/implementations')
+        describe "when passed 'core'" do
+          it "returns a Dir representing the JsTestCore core directory" do
+            runner = web_root.locate('core')
+            runner.should == Resources::Dir.new(JsTestCore::Server.core_path, '/core')
+          end
         end
 
-        it "when passed 'results', returns a Suite" do
-          runner = web_root.locate('suites')
-          runner.should == Resources::Suite
+        describe "when passed 'implementations'" do
+          it "returns a Dir representing the javascript implementations directory" do
+            runner = web_root.locate('implementations')
+            runner.should == Resources::Dir.new(JsTestCore::Server.implementation_root_path, '/implementations')
+          end
         end
 
-        it "when passed 'runners', returns a Runner" do
-          runner = web_root.locate('runners')
-          runner.should be_instance_of(Resources::Runners)
+        describe "when passed 'results'" do
+          it "returns a Suite" do
+            runner = web_root.locate('suites')
+            runner.should == Resources::Suite
+          end
         end
 
-        it "when passed a directory that is in the public_path, returns a Dir representing that directory" do
-          runner = web_root.locate('stylesheets')
-          runner.should == Resources::Dir.new("#{JsTestCore::Server.public_path}/stylesheets", '/stylesheets')
+        describe "when passed 'runners'" do
+          it "returns a Runner" do
+            runner = web_root.locate('runners')
+            runner.should be_instance_of(Resources::Runners)
+          end
         end
 
-        it "when passed a file that is in the public_path, returns a File representing that file" do
-          runner = web_root.locate('robots.txt')
-          runner.should == Resources::File.new("#{JsTestCore::Server.public_path}/robots.txt", '/robots.txt')
+        describe "when passed a directory that is in the public_path" do
+          it "returns a Dir representing that directory" do
+            runner = web_root.locate('stylesheets')
+            runner.should == Resources::Dir.new("#{JsTestCore::Server.public_path}/stylesheets", '/stylesheets')
+          end
         end
 
-        it "when not passed 'core' or 'specs', raises an error" do
-          lambda do
-            web_root.locate('invalid')
-          end.should raise_error
+        describe "when passed a file that is in the public_path" do
+          it "returns a File representing that file" do
+            runner = web_root.locate('robots.txt')
+            runner.should == Resources::File.new("#{JsTestCore::Server.public_path}/robots.txt", '/robots.txt')
+          end
+        end
+
+        describe "when passed an invalid option" do
+          it "raises an error" do
+            lambda do
+              web_root.locate('invalid')
+            end.should raise_error
+          end
         end
       end
 
       describe ".dispatch_specs" do
-        it "causes #locate /specs to dispatch to a Spec::SpecDir" do
-          WebRoot.dispatch_specs
-          
-          resource = web_root.locate('specs')
-          resource.should == spec_dir('')
+        describe "#get" do
+          attr_reader :request, :response
+          before do
+            @request = Rack::Request.new({'rack.input' => StringIO.new("")})
+            @response = Rack::Response.new
+          end
+
+          it "redirects to /specs" do
+            WebRoot.dispatch_specs
+
+            web_root.get(request, response)
+            response.should be_redirect
+            response.headers["Location"].should == "/specs"
+          end
         end
+
+        describe "#locate /specs" do
+          it "dispatches to a Spec::SpecDir" do
+            WebRoot.dispatch_specs
+
+            resource = web_root.locate('specs')
+            resource.should == spec_dir('')
+          end
+        end
+
       end
 
       describe "when .dispatch_specs is not called" do
