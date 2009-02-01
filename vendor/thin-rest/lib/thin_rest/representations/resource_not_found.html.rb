58f55f28e84b0dc8b6ae4118546c5d463d182f24
Brian Takita <btakita@btakita-laptop.(none)>
2009-01-31 22:26:22 -0500

    Braid: Updated mirror 'vendor/thin-rest' to '61351ed'

--- vendor/thin-rest/lib/thin_rest/representations/resource_not_found.html.rb
+++ vendor/thin-rest/lib/thin_rest/representations/resource_not_found.html.rb
@@ -1,16 +1,18 @@
-module Representations
-  class ResourceNotFound < Page
-    protected
-    def body_content
-      h1 message
-    end
+module ThinRest
+  module Representations
+    class ResourceNotFound < Page
+      protected
+      def body_content
+        h1 message
+      end
 
-    def title_text
-      message
-    end
+      def title_text
+        message
+      end
 
-    def message
-      "File #{path} not found"
+      def message
+        "File #{path} not found"
+      end
     end
   end
 end
